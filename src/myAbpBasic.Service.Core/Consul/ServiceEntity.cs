315d9f3f15782d8234d400c089013b5333e261fe
rock <rockcode777@gmail.com>
2019-04-01 16:17:42 +0800

    abp整合consul

---
+++ src/myAbpBasic.Service.Core/Consul/ServiceEntity.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+
+namespace myAbpBasic.Service.Core.Consul
+{
+    public class ServiceEntity
+    {
+
+        public ServiceEntity(IConfiguration configuration)
+        {
+            IP = configuration["Service:IP"];
+            Port = Convert.ToInt32(configuration["Service:Port"]);
+            ServiceName = configuration["Service:ServiceName"];
+            ConsulIP = configuration["Service:ConsulIP"];
+            ConsulPort = Convert.ToInt32(configuration["Consul:Port"]);
+        }
+
+        public string IP { get; set; }
+        public int Port { get; set; }
+        public string ServiceName { get; set; }
+        public string ConsulIP { get; set; }
+        public int ConsulPort { get; set; }
+    }
+}
---
+++ src/myAbpBasic.WebApi/Controllers/HealthController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace myAbpBasic.WebApi.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Health")]
+    public class HealthController : Controller
+    {
+        [HttpGet]
+        public IActionResult Get() => Ok("ok");
+    }
+}
